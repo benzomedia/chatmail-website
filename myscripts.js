798c11741607e024a14d952c9f087dd01d5eafbf
Tom Reuveni <tomreuveni@Toms-MacBook-Pro.local>
2017-04-02 11:10:27 +0300

    first commit

---
+++ myscripts.js
@@ -0,0 +1,11 @@
+
+// window.onscroll = function() {myFunction()};
+//
+// function myFunction() {
+//     if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+//         var elements = document.getElementsByClassName("bullet")
+//         for(var i = 0; i < elements.length; i++) {
+//           elements[i].className = 'slideUp'
+//         }
+//     }
+// }
